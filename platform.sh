c300966522544a249213e42b1dd4d826c1da5e23
Anup tripathi <imanuptripathi@gmail.com>
2020-07-23 16:57:09 +0000

    adding automation script

---
+++ platform.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.12.2-linux-amd64.tar.gz
+sudo tar xvf helm-v2.12.2-linux-amd64.tar.gz
+cd linux-amd64 && sudo cp helm /usr/local/bin/
+
+cd ~/
+kubectl create serviceaccount tiller --namespace kube-system
+sleep 10
+kubectl apply -f manifest/helm-rbac-config.yaml
+sleep 10
+helm init --wait --service-account tiller
+sleep 10
+
+kubectl create ns development
+
+kubectl apply -f guest-book/redis-master-deployment.yaml
+kubectl apply -f guest-book/redis-master-service.yaml
+kubectl apply -f guest-book/redis-slave-deployment.yaml
+kubectl apply -f guest-book/redis-slave-service.yaml
+kubectl apply -f guest-book/frontend-deployment.yaml
+kubectl apply -f guest-book/frontend-service.yaml
+kubectl apply -f guest-book/app-gateway.yaml
+
+##### Istio
+sleep 20
+helm repo add istio.io https://storage.googleapis.com/istio-release/releases/1.1.4/charts/
+helm install istio.io/istio-init --name istio-init --version 1.1.4 --namespace istio-system --wait
+sleep 20
+
+helm install istio.io/istio --name istio --version 1.1.4 --namespace istio-system
+
+sleep 20
+kubectl label namespace default istio-injection=enabled --overwrite
+
+kubectl create ns monitoring
+
+helm install \
+    --namespace=monitoring \
+    --name=prometheus \
+    stable/prometheus
+
+helm install \
+    --namespace=monitoring \
+    --name=grafana \
+    --set=adminUser=admin \
+    --set=adminPassword=admin \
+    --set=service.type=NodePort \
+    stable/grafana
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
